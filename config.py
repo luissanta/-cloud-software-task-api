543617c9a430ef980dbcd9ce3b89b4b439ffeac1
Luis <luisfernandosantaospina@gmail.com>
2023-04-15 08:36:22 -0500

    Add wsgi config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     JWT_SECRET_KEY = 'SFDJKGKJFD7SG987FDS?9889'
     STATIC_FOLDER = "views/static/"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///task.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///D:///task.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
     DEBUG = False
