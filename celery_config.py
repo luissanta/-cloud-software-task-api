6390bb0559a859565cd8d3f2019cf9607b584998
Luis Santa <l.santa@uniandes.edu.co>
2023-04-16 13:24:12 +0000

    Add redis

--- celery_config.py
+++ celery_config.py
@@ -1,5 +1,5 @@
-broker_url='redis://localhost:6379/0'
-result_backend='redis://localhost:6379/0'
+broker_url='redis://:G8heLqPFiLWURyiEcmvYwaoICVvLt0xjZAzCaPwPnJ8=@myredis2.redis.cache.windows.net:6379/0'
+result_backend='redis://:G8heLqPFiLWURyiEcmvYwaoICVvLt0xjZAzCaPwPnJ8=@myredis2.redis.cache.windows.net:6379/0'
 task_serializer='json'
 result_serializer='json'
 imports=(
