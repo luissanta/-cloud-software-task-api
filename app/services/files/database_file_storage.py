b7da0991cd8a8458523a15d0b9f4f58f72268337
Luis <luisfernandosantaospina@gmail.com>
2023-04-25 07:08:32 -0500

    Fix syntax

--- app/services/files/database_file_storage.py
+++ app/services/files/database_file_storage.py
@@ -4,14 +4,16 @@ from app.models.models import File
 from app.databases import db
 from .i_File import IFile
 
+
 class FileTypeEnum(Enum):
     ORIGINAL = 'ORIGINAL'
     COMPRESSED = 'COMPRESSED'
 
+
 class DatabaseFileStorage(IFile):
 
-    def extract_name(self, file, type):        
-        if type  == FileTypeEnum.ORIGINAL.value:
+    def extract_name(self, file, type_file):
+        if type_file == FileTypeEnum.ORIGINAL.value:
             return file.original_name, file.original_data
         else:
             tmp_name = file.original_name
@@ -19,13 +21,18 @@ class DatabaseFileStorage(IFile):
             tmp_name = tmp_name+"."+file.new_format
             return tmp_name,  file.compressed_data
         
-    def get(self, id, type) -> tuple:
-        fetched_file = File.query.get_or_404(id)
-        name, data = self.extract_name(fetched_file, type)
-        return data , name       
+    def get(self, file_id, type_file) -> tuple:
+        fetched_file = File.query.get_or_404(file_id)
+        name, data = self.extract_name(fetched_file, type_file)
+        return data, name
     
-    def save(self,name_file,file_data, new_format) -> int:
-        upload_file = File(original_data=file_data, original_name=name_file, created_at=func.now(), new_format = new_format)
+    def save(self, name_file, file_data, new_format) -> int:
+        upload_file = File(
+            original_data=file_data,
+            original_name=name_file,
+            created_at=func.now(),
+            new_format=new_format
+        )
         db.session.add(upload_file)
         db.session.commit()
-        return upload_file.id 
+        return upload_file.id
--- app/services/files/network_file_storage.py
+++ app/services/files/network_file_storage.py
@@ -3,59 +3,75 @@ import uuid
 from sqlalchemy import func
 from app.models.models import File
 from app.databases import db
-from smb.SMBConnection import SMBConnection  
+from smb.SMBConnection import SMBConnection
 from .i_File import IFile
 import os
 from enum import Enum
+
 
 class FileTypeEnum(Enum):
     ORIGINAL = 'ORIGINAL'
     COMPRESSED = 'COMPRESSED'
 
+
 class NetworkFileStorage(IFile):
     smb_username = os.getenv("smb_username", "")
-    smb_password =  os.getenv("smb_password", "")
-    smb_server_ip = os.getenv("smb_server_ip", "") 
+    smb_password = os.getenv("smb_password", "")
+    smb_server_ip = os.getenv("smb_server_ip", "")
     smb_folder_name = os.getenv("smb_folder_name", "")
 
-    def extract_name(self, file, type):    
-        print(type.file_type)    
-        if type.file_type == FileTypeEnum.ORIGINAL.value:
+    def extract_name(self, file, type_file):
+        if type_file.file_type == FileTypeEnum.ORIGINAL.value:
             name = file.original_name.split('.')
-            return file.original_name, "\\public\\original\\", name[1] 
+            return file.original_name, "\\public\\original\\", name[1]
         else:
             tmp_name = file.original_name
             tmp_name = tmp_name.split('.')
-            tmp_name = tmp_name[0]+"."+file.new_format
-            return tmp_name, "\\public\\compressed\\"  ,file.new_format
-        
-    def get(self,id, type) -> tuple:                
-        fetched_file = File.query.get_or_404(id)
-        original_name, path, extension = self.extract_name(fetched_file, type)
-        conn = SMBConnection(self.smb_username, self.smb_password,remote_name='',my_name='', use_ntlm_v2=True, is_direct_tcp=True)
-        conn.connect(self.smb_server_ip, 445)     
-        memoria = BytesIO()    
-        file_obj = conn.retrieveFile(self.smb_folder_name, path+fetched_file.temporal_name+"."+extension, memoria)    
+            tmp_name = tmp_name[0] + "." + file.new_format
+            return tmp_name, "\\public\\compressed\\", file.new_format
+
+    def get(self, file_id, file_type) -> tuple:
+        fetched_file = File.query.get_or_404(file_id)
+        original_name, path, extension = self.extract_name(fetched_file, file_type)
+        conn = SMBConnection(
+            self.smb_username,
+            self.smb_password,
+            remote_name='',
+            my_name='',
+            use_ntlm_v2=True,
+            is_direct_tcp=True
+        )
+        conn.connect(self.smb_server_ip, 445)
+        memoria = BytesIO()
+        file_obj = conn.retrieveFile(self.smb_folder_name, path + fetched_file.temporal_name + "." + extension, memoria)
         memoria.seek(0)
         data = memoria.read()
         memoria.close()
-        conn.close()               
-        
-        return data , original_name        
+        conn.close()
 
+        return data, original_name
 
-    def save(self,name_file,file_data, new_format) -> int:        
+    def save(self, name_file, file_data, new_format) -> int:
         temporal_name = str(uuid.uuid4())
         temp_original_name = name_file.split('.')
-        conn = SMBConnection(self.smb_username, self.smb_password,remote_name='',my_name='', use_ntlm_v2=True, is_direct_tcp=True)
-        try:            
-            conn.connect(self.smb_server_ip, 445)            
-            conn.storeFile(self.smb_folder_name, "\\public\\original\\"+temporal_name+"."+temp_original_name[1],BytesIO(file_data))            
+        conn = SMBConnection(
+            self.smb_username,
+            self.smb_password,
+            remote_name='',
+            my_name='',
+            use_ntlm_v2=True,
+            is_direct_tcp=True
+        )
+        try:
+            conn.connect(self.smb_server_ip, 445)
+            conn.storeFile(self.smb_folder_name, "\\public\\original\\" + temporal_name + "." + temp_original_name[1],
+                           BytesIO(file_data))
         except Exception as e:
-           print(f"Detail error: {e}")
+            print(f"Detail error: {e}")
 
         conn.close()
-        upload_file = File(original_name=name_file, created_at=func.now(), temporal_name = temporal_name, new_format = new_format)
+        upload_file = File(original_name=name_file, created_at=func.now(), temporal_name=temporal_name,
+                           new_format=new_format)
         db.session.add(upload_file)
         db.session.commit()
-        return upload_file.id  
+        return upload_file.id
